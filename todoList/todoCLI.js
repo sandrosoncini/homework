d3e6c69d414a1acbd7e10b3616099f21eb1b4c6d
Sandro Soncini <sandrosoncini@hotmail.com>
2019-12-13 13:05:47 -0800

    with coments

--- todoList/todoCLI.js
+++ todoList/todoCLI.js
@@ -4,17 +4,19 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+let list = []// list of tasks
 
+//calling the function the user wants the menu access.
 const todoList = answer =>{
     if( answer === 'v'){
         view();
     }else if (answer === 'n'){
         add();
-    }else if (answer[0] === 'c' ){
+    }else if (answer[0] === 'c' && answer[1] === typeof 'number' ){
         number= answer[1];
         complete(number);
         menu();
-    }else if (answer[0] === 'd'){
+    }else if (answer[0] === 'd'&& answer[1] === typeof 'number'){
         number= answer[1];
         del(number);
         menu();
@@ -25,7 +27,10 @@ const todoList = answer =>{
         menu();
     }
 };
-let count = 0;
+
+// 
+let count = 0;// count if is the Welcome menu
+//menu questions
 let menu = () => {
     if(count === 0){
         count++
@@ -36,10 +41,10 @@ let menu = () => {
     }else{
     rl.question(`\n(v) View • (n) New • (cX) Complete • (dX) Delete • (q) Quit\n>`,todoList)};
     }
- 
 
-let list = []
 menu()
+
+//view list
 function view () {
     if(list.length === 0){
         console.log(`\nList is empty...`)
@@ -53,25 +58,44 @@ function view () {
         menu();
 };
 
+// add to the list
 function add(){
     rl.question(`\nWhat?\n>`, (answer)=>{
         list.push({task: answer, done: false})
         menu();
     });
 };
+
+//complete task of the list
 function complete (number){
+    if(number > list.length){
+        console.log(`\nInvalid Number\n`);
+        view()
+    }else {
     list[number].done = true
     console.log(`\nCompleted ${list[number].task}\n`)
     menu();
+    }
+    
 };
+
+// delete task of the list
 function del (number) {
+    if(number > list.length){
+        console.log(`\nInvalid Number\n`);
+        menu()
+    }else{
     let task=list[number].task
     list.splice(number, 1); 
     console.log(`\nDeleted ${task}\n`);
-    menu();
+    view();
+    }
 };
+
+//quit the list
 function quit (){
     rl.close();
+    console.log(`\nThanks. See you soon!\n`)
 };
 
 
